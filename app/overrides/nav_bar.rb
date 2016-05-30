a258dd233f2fe2c04e62388eacc7eda08feee87f
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-30 19:44:41 -0400

    create override for topmost header section

---
+++ app/overrides/nav_bar.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path  => "spree/shared/_nav_bar",
+                     :replace => "nav.col-md-12",
+                     :partial          => "spree/shared/_nav_bar",
+                     :name          => "pages_nav")
