a62e894a71c541b47e2b77bcba6c0035adc657af
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-06-25 18:19:54 -0400

    create override for _main_nav_bar partial to use test version

--- app/overrides/main_nav_bar.rb
+++ app/overrides/main_nav_bar.rb
@@ -1,4 +1,7 @@
 Deface::Override.new(:virtual_path  => "spree/shared/_main_nav_bar",
                      :replace => "nav.col-md-12",
-                     :partial          => "spree/shared/_main_nav_bar",
+                     :partial          => "spree/shared/_main_nav_bar_test",
                      :name          => "pages_nav")
+# TODO:
+# - re implement this partial and fix the whole taxonomies thing
+# - For some reason this file is causing all the damn problems
